abef130d6da0e41a874bd1d311d8cd2e803a43ae
Javier Cruz López de Ochoa <javcruz@ucm.es>
2020-10-20 19:08:56 +0200

    Mapa en ghosts

--- PracticasICI/src/main/java/es/ucm/fdi/ici/c2021/practica0/grupo09/Ghosts.java
+++ PracticasICI/src/main/java/es/ucm/fdi/ici/c2021/practica0/grupo09/Ghosts.java
@@ -1,54 +1,128 @@
 package es.ucm.fdi.ici.c2021.practica0.grupo09;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Random;
 
 import pacman.controllers.GhostController;
-import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
+import pacman.game.internal.Node;
 
 public final class Ghosts extends GhostController {
-    private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
-    private MOVE[] allMoves = MOVE.values();
-    private Random rnd = new Random();
 
-    @Override
-    public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
-        moves.clear();
-        for (GHOST ghostType : GHOST.values()) {
-            if (game.doesGhostRequireAction(ghostType)) {
-                moves.put(ghostType, getGhostMove(game, timeDue, ghostType));
-            }
-        }
-        return moves;
-    }
+	//Variables del mapa ---------------------------------------------------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	class interseccion {
+		public interseccion(int iden, EnumMap<MOVE, Integer> dir, EnumMap<MOVE, Integer> dest,
+				EnumMap<MOVE, Integer> pi, EnumMap<MOVE, Integer> ppi) {
+			identificador = iden;
+			direccions = dir;
+			destinos = dest;
+			pills = pi;
+			powePill = ppi;
+		}
 
-    private MOVE getGhostMove(Game game, long timeDue, GHOST ghostType) {
-        if (game.isGhostEdible(ghostType) || isPacManCloseToPowerPill(game, 20)) {
-            return game.getApproximateNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghostType),
-                    game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghostType), DM.EUCLID);
-        }
+		public int identificador; // node index
+		public EnumMap<MOVE, Integer> direccions; // distancias
+		public EnumMap<MOVE, Integer> destinos; // identificador del nodo en esa direccion
+		public EnumMap<MOVE, Integer> pills; // pills en ese camino
+		public EnumMap<MOVE, Integer> powePill; // powerPills en ese camino
+	}
+	private List<interseccion> mapa = new ArrayList<interseccion>();
+	boolean mapaHecho = false;
 
-        float rng = rnd.nextFloat();
+	private int[] buscaCamino(Node nodoActual, MOVE dir, Node[] graph) {
+		MOVE direccion = dir;
+		int pills = 0;
+		int powerPills = 0;
 
-        if (rng < 0.9f)
-            return game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghostType),
-                    game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghostType), DM.EUCLID);
-        else
-            return allMoves[rnd.nextInt(allMoves.length)];
-    }
+		Node proximoNodo = graph[nodoActual.neighbourhood.get(direccion)];
+		int coste = 1;
 
-    private boolean isPacManCloseToPowerPill(Game game, int limit) {
-        int pacmanPos = game.getPacmanCurrentNodeIndex();
-        double closestDistance = Double.MAX_VALUE;
-        for (int currentPill : game.getActivePowerPillsIndices()) {
-            double aux = game.getDistance(pacmanPos, currentPill, DM.EUCLID);
-            if (aux < closestDistance) {
-                closestDistance = aux;
-            }
-        }
-        return closestDistance < limit;
-    }
+		while ((proximoNodo.numNeighbouringNodes <= 2)) {
+			if (proximoNodo.neighbourhood.get(direccion) == null) { // en que otra direccion nos podemos mover
+				for (MOVE m : MOVE.values()) { // ya tenemos la nueva direccion
+					if (m != direccion.opposite() && proximoNodo.neighbourhood.get(m) != null) {
+						direccion = m;
+						break;
+					}
+				}
+			}
+			if (proximoNodo.pillIndex != -1)
+				pills++;
+			else if (proximoNodo.powerPillIndex != -1)
+				powerPills++;
+			proximoNodo = graph[proximoNodo.neighbourhood.get(direccion)];
+			coste++;
+		}
+		return new int[] { coste, proximoNodo.nodeIndex, pills, powerPills };
+	}
+	private void crearMapa(Game game) {
+		Node[] graph = game.getCurrentMaze().graph;
+		for (Node nodo : graph) { // recorre todos los nodos del mapa
+			if (nodo.numNeighbouringNodes > 2) { // quita muro y pasillos
+
+				// miramos todas las direcciones posibles
+				EnumMap<MOVE, Integer> map = nodo.neighbourhood;
+
+				EnumMap<MOVE, Integer> direcciones = new EnumMap<MOVE, Integer>(MOVE.class);
+				EnumMap<MOVE, Integer> destinations = new EnumMap<MOVE, Integer>(MOVE.class);
+				EnumMap<MOVE, Integer> pills = new EnumMap<MOVE, Integer>(MOVE.class);
+				EnumMap<MOVE, Integer> powerPills = new EnumMap<MOVE, Integer>(MOVE.class);
+
+				for (MOVE m : MOVE.values()) {
+					if (map.get(m) != null) { // direccion existente
+						int[] temp = buscaCamino(nodo, m, graph);
+						direcciones.put(m, temp[0]);
+						destinations.put(m, temp[1]);
+						pills.put(m, temp[2]);
+						powerPills.put(m, temp[3]);
+					}
+				}
+				mapa.add(new interseccion(nodo.nodeIndex, direcciones, destinations, pills, powerPills));
+			}
+		}
+	}
+	private interseccion interseccion_rec(int ini, int fin, int iden) {
+		if (fin - ini > 1) { // sigue habiendo varios nodos en el rango de búsqueda
+			int mid = (fin - ini) / 2 + ini;
+			if (mapa.get(mid).identificador <= iden)
+				return interseccion_rec(mid, fin, iden); // esta en el lado derecho
+			else
+				return interseccion_rec(ini, mid, iden); // esta en el lado izquierdo
+		} else
+			return mapa.get(ini); // es de tamaño 1 por tanto devuelve el elemento inicial
+	}
+	private interseccion getInterseccion(int iden) { // usa divide y venceras para encontrar la interseccion
+		return interseccion_rec(0, mapa.size(), iden);
+	}
+
+	//Variables del controlador --------------------------------------------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+	//Ghosts orden: rojo, rosa, azul, amarillo
+
+	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);	
+	private MOVE[] allMoves = MOVE.values();
+	private Random rnd = new Random();
+
+	@Override
+	public final EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		if (!mapaHecho) { // solo entra aqui en el primer ciclo
+			crearMapa(game);
+			mapaHecho = true;
+		}
+		moves.clear();
+		for (GHOST ghostType : GHOST.values()) {
+			if (game.doesGhostRequireAction(ghostType)) {
+				moves.put(ghostType, allMoves[rnd.nextInt(allMoves.length)]);
+			}
+		}
+
+		return moves;
+	}
 }
