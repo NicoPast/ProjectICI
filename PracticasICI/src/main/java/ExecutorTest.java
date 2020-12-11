5bedf54ae1e0a56655d35875718339828bbb5608
JuanmaSalva <jsalva@ucm.es>
2020-12-11 15:49:58 +0100

    Ejemplo fuzzy instalado

---
+++ PracticasICI/src/main/java/ExecutorTest.java
@@ -0,0 +1,25 @@
+import es.ucm.fdi.ici.practica4.demofuzzy.MsPacManFuzzy;
+import pacman.Executor;
+import pacman.controllers.GhostController;
+import pacman.controllers.PacmanController;
+
+public class ExecutorTest {
+
+    public static void main(String[] args) {
+        Executor executor = new Executor.Builder()
+                .setTickLimit(4000)
+                .setGhostPO(true)
+                .setPacmanPO(true)
+                .setVisual(true)
+                .setScaleFactor(3.0)
+                .build();
+
+        PacmanController pacMan = new MsPacManFuzzy();
+        GhostController ghosts = new GhostsRandom();
+        
+        System.out.println( 
+        		executor.runGame(pacMan, ghosts, 40)
+        );
+        
+    }
+}
