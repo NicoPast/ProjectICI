c5c9c04ab79f1f338831af36bf3bc23e55310219
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2021-01-01 11:11:41 +0100

    relleno el input y runAway adaptado

--- PracticasICI/src/main/java/es/ucm/fdi/ici/c2021/practica4/grupo09/ghosts/GhostsInput.java
+++ PracticasICI/src/main/java/es/ucm/fdi/ici/c2021/practica4/grupo09/ghosts/GhostsInput.java
@@ -17,94 +17,106 @@ import pacman.game.Game;
 public class GhostsInput implements Input {
 
 	public HashMap<String, Double> getFuzzyValues() {
-		HashMap<String,Double> vars = new HashMap<String,Double>();
+		HashMap<String, Double> vars = new HashMap<String, Double>();
 
 		return vars;
 	}
 
 	private GHOST ghost;
-	public class UsefulData{
+
+	public class UsefulData {
 		public Vector<interseccion> GhostsPositions;
-		public Vector<Double>GhostsPositionsAccuracy;
-		public Vector<MOVE>GhostsLastMoveMade;
-		
-		public Vector<Double>GhostIsEdibleAccuracy;
+		public Vector<Double> GhostsPositionsAccuracy;
+		public Vector<MOVE> GhostsLastMoveMade;
+
+		public Vector<Double> GhostIsEdibleAccuracy;
 
 		public interseccion proximaInterseccionPacMan;
 		public double proximaInterseccionPacManAccuracy;
 		public MOVE PacmanLastMoveMade;
-		
-		UsefulData(Vector<interseccion>Ghosts,Vector<Double>GhostFuzzy,Vector<Double>EdibleAccuracy,interseccion Pacman, double PacmanAccuracy,
-				Vector<MOVE>GhostsMoves, MOVE PacmanMove)
-		{
-			GhostsPositions=Ghosts;
-			GhostsPositionsAccuracy=GhostFuzzy;
-			GhostIsEdibleAccuracy=EdibleAccuracy;
-			proximaInterseccionPacMan=Pacman;
-			proximaInterseccionPacManAccuracy=PacmanAccuracy;
-			GhostsLastMoveMade=GhostsMoves;
-			PacmanLastMoveMade=PacmanMove;
+
+		UsefulData(Vector<interseccion> Ghosts, Vector<Double> GhostFuzzy, Vector<Double> EdibleAccuracy,
+				interseccion Pacman, double PacmanAccuracy, Vector<MOVE> GhostsMoves, MOVE PacmanMove) {
+			GhostsPositions = Ghosts;
+			GhostsPositionsAccuracy = GhostFuzzy;
+			GhostIsEdibleAccuracy = EdibleAccuracy;
+			proximaInterseccionPacMan = Pacman;
+			proximaInterseccionPacManAccuracy = PacmanAccuracy;
+			GhostsLastMoveMade = GhostsMoves;
+			PacmanLastMoveMade = PacmanMove;
 		}
 	}
-	
-	public UsefulData getData(){return new UsefulData(this.GhostsPositions,this.GhostsPositionsAccuracy,this.GhostIsEdibleAccuracy,this.proximaInterseccionPacMan,
-			this.proximaInterseccionPacManAccuracy,this.GhostsLastMoveMade,this.PacmanLastMoveMade);}
-	
-	
+
+	public UsefulData getData() {
+		return new UsefulData(this.GhostsPositions, this.GhostsPositionsAccuracy, this.GhostIsEdibleAccuracy,
+				this.PosPacMan, this.PosPacManAccuracy, this.GhostsLastMoveMade, this.PacmanLastMoveMade);
+	}
+
 	private MapaInfoGhost mapa;
 
-	
 	private Vector<interseccion> GhostsPositions;
-	private Vector<Double>GhostsPositionsAccuracy;
-	private Vector<MOVE>GhostsLastMoveMade;
-	
-	
-	private Vector<Double>GhostIsEdibleAccuracy;
+	private Vector<Double> GhostsPositionsAccuracy;
+	private Vector<MOVE> GhostsLastMoveMade;
 
-	private interseccion proximaInterseccionPacMan;
-	private double proximaInterseccionPacManAccuracy;
+	private Vector<Double> GhostIsEdibleAccuracy;
+
+	private interseccion PosPacMan;
+	private double PosPacManAccuracy;
 	private MOVE PacmanLastMoveMade;
-	
-	
-	
-	public void setGhost(GHOST g) {ghost=g;}
+
+	public void setGhost(GHOST g) {
+		ghost = g;
+	}
+
 	public GhostsInput(MapaInfoGhost mapaInfo) {
 		this.mapa = mapaInfo;
-		this.GhostsPositions=new Vector<interseccion>(4);
-		this.GhostsPositionsAccuracy=new Vector<Double>(4);
-		this.GhostsLastMoveMade=new Vector<MOVE>(4);
-		this.GhostIsEdibleAccuracy=new Vector<Double>(4);
-		this.proximaInterseccionPacMan = new interseccion(-1, new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class),
-				new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class));
-		this.proximaInterseccionPacManAccuracy=0.0;
-		this.PacmanLastMoveMade=MOVE.NEUTRAL;
-		//las inicializamos a valores desconocidos
-		for(int i=0; i<4;i++) {
-			this.GhostsPositions.set(i,new interseccion(-1, new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class),
-					new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class)));
-			
+		this.GhostsPositions = new Vector<interseccion>(4);
+		this.GhostsPositionsAccuracy = new Vector<Double>(4);
+		this.GhostsLastMoveMade = new Vector<MOVE>(4);
+		this.GhostIsEdibleAccuracy = new Vector<Double>(4);
+		this.PosPacMan = new interseccion(-1, new EnumMap<MOVE, Integer>(MOVE.class),
+				new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class),
+				new EnumMap<MOVE, Integer>(MOVE.class));
+		this.PosPacManAccuracy = 0.0;
+		this.PacmanLastMoveMade = MOVE.NEUTRAL;
+		// las inicializamos a valores desconocidos
+		for (int i = 0; i < 4; i++) {
+			this.GhostsPositions.set(i,
+					new interseccion(-1, new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class),
+							new EnumMap<MOVE, Integer>(MOVE.class), new EnumMap<MOVE, Integer>(MOVE.class)));
+
 			this.GhostsPositionsAccuracy.set(i, 0.0);
 			this.GhostsLastMoveMade.set(i, MOVE.NEUTRAL);
 			this.GhostIsEdibleAccuracy.set(i, 0.0);
 		}
 	}
-	
 
 	@Override
 	public void parseInput(Game game) {
-		
-		mapa.update(game);
-		for	(int i=0;i<4;i++)
-		{
-			int fantasmaNode=game.getGhostCurrentNodeIndex(GHOST.values()[i]);
-			if(fantasmaNode>-1)
-				this.GhostsPositions.set(i,mapa.getInterseccion(fantasmaNode));
-		}
-			
-		
-		
-	}
 
-	
+		mapa.update(game);
+		for (int i = 0; i < 4; i++) {
+			int fantasmaNode = game.getGhostCurrentNodeIndex(GHOST.values()[i]);
+			if (fantasmaNode > -1) {
+				GhostsPositions.set(i, mapa.getInterseccion(fantasmaNode));
+				GhostsPositionsAccuracy.set(i, 1.0);
+				this.GhostsLastMoveMade.set(i, game.getGhostLastMoveMade(GHOST.values()[i]));
+			} else
+				GhostsPositionsAccuracy.set(i, GhostsPositionsAccuracy.elementAt(i) - .1);
+			Boolean edi = game.isGhostEdible(GHOST.values()[i]);
+			if (edi != null) {
+				GhostIsEdibleAccuracy.set(i, 1.0);
+			} else
+				GhostIsEdibleAccuracy.set(i, GhostIsEdibleAccuracy.elementAt(i) - .2);
+		}
+		int PacmanP = game.getPacmanCurrentNodeIndex();
+		if (PacmanP > -1) {
+			this.PosPacMan = mapa.getInterseccion(PacmanP);
+			this.PosPacManAccuracy = 1.0;
+			this.PacmanLastMoveMade = game.getPacmanLastMoveMade();
+		} else
+			this.PosPacManAccuracy -= .1;
+
+	}
 
 }
--- PracticasICI/src/main/java/es/ucm/fdi/ici/c2021/practica4/grupo09/ghosts/actions/RunAwayAction.java
+++ PracticasICI/src/main/java/es/ucm/fdi/ici/c2021/practica4/grupo09/ghosts/actions/RunAwayAction.java
@@ -14,56 +14,68 @@ public class RunAwayAction implements Action {
 
 	GHOST ghost;
 	MapaInfoGhost mymapa;
-
-	public RunAwayAction(GHOST ghost, MapaInfoGhost map) {
+	Vector<Float> EdibleGhostsPosAccuracies;
+	Vector<interseccion> LastGhostsKnownPositions;
+	int PacmanLastPosKnown;
+	float PacmanPosAccuracy;
+	float PACMAN_POS_ACCURACY_LIMIT = .5f;
+	float POS_ACCURACY_LIMIT = .7f;
+	public RunAwayAction(GHOST ghost, MapaInfoGhost map,Vector<Float> Edibles,Vector<interseccion> LastPos, int PacmanPos, float PacmanAccur) {
 		this.ghost = ghost;
 		mymapa = map;
+		this.EdibleGhostsPosAccuracies = Edibles;
+		this.LastGhostsKnownPositions = LastPos;
+		this.PacmanLastPosKnown = PacmanPos;
+		this.PacmanPosAccuracy = PacmanAccur;
 	}
 	
-	private Vector<GHOST> edibleGhosts(Game game) {
-		Vector<GHOST> edibleGhosts = new Vector<GHOST>();
-		for (GHOST ghost : GHOST.values()) {
-			if (game.isGhostEdible(ghost))
-				edibleGhosts.add(ghost);
-		}
-		return edibleGhosts;
-	}
+//	private Vector<GHOST> edibleGhosts(Game game) {
+//		Vector<GHOST> edibleGhosts = new Vector<GHOST>();
+//		for (GHOST ghost : GHOST.values()) {
+//			if (game.isGhostEdible(ghost))
+//				edibleGhosts.add(ghost);
+//		}
+//		return edibleGhosts;
+//	}
 
-	private double furthestGhostDistance(int[] pos, MOVE m, Game game) {
+	private double furthestGhostDistance(MOVE m, Game game) {
 
-		double furthest = 0;
-		for (int p : pos) {
+		double furthest = -1;
+		for (int i=0;i<4;i++) {
+			if(GHOST.values()[i]==ghost)
+				continue;
+			
+			if (LastGhostsKnownPositions.elementAt(i).identificador > -1
+					&& EdibleGhostsPosAccuracies.elementAt(i) >= POS_ACCURACY_LIMIT
+					) { 
 			double aux = game.getDistance(game.getGhostCurrentNodeIndex(ghost), p, m, DM.PATH);
 			if (aux > furthest) {
 				furthest = aux;
 							}
 		}
 
+			}
 		return furthest;
 	}
 
 	@Override
 	public MOVE execute(Game game) {
+		MOVE bestMove =MOVE.NEUTRAL;
 		if (game.doesGhostRequireAction(ghost)) 
 		{
-			MOVE prohibido = game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghost),
-					game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghost),
+			
+			MOVE prohibido = MOVE.NEUTRAL;
+			if(PacmanPosAccuracy > PACMAN_POS_ACCURACY_LIMIT) {
+				
+			prohibido=game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghost),
+					PacmanLastPosKnown, game.getGhostLastMoveMade(ghost),
 					DM.EUCLID);
-			MOVE bestMove = game.getApproximateNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghost),
-					game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghost),DM.EUCLID);
+			bestMove = game.getApproximateNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghost),
+					PacmanLastPosKnown, game.getGhostLastMoveMade(ghost),DM.EUCLID);
+			}
 			interseccion inter = mymapa.getInterseccion(game.getGhostCurrentNodeIndex(ghost));
 			if (inter != null) {
-				int[] posGhosts = new int[3];
-				int i = 0;
-				i = 0;
-				// rellenamos las posiciones de los fantasmas que se pueden comer para huir de
-				// ellos
-				for (GHOST g : edibleGhosts(game)) {
-					if (g == ghost)
-						continue;
-					posGhosts[i] = game.getGhostCurrentNodeIndex(g);
-					i++;
-				}
+				
 				// buscamos el fantasma mas lejano en todas direcciones excepto
 				// la prohibida y vamos a por ese
 				double furthest = 0;
@@ -71,16 +83,16 @@ public class RunAwayAction implements Action {
 					if (move == prohibido)
 						continue;
 
-					double aux = furthestGhostDistance(posGhosts, move, game);
+					double aux = furthestGhostDistance(move, game);
 					if (aux > furthest) {
 						furthest = aux;
+						if(furthest>-1)
 						bestMove = move;
 					}
 				}
 			}
-			return bestMove;
-		} else
-			return MOVE.NEUTRAL;
+		} 
+		return bestMove;
 
 	}
 }
