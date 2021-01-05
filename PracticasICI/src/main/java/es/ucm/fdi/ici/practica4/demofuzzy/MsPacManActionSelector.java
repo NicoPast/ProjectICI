20f07c0f50645e2abf8a99f0db899d4a16a2f42a
JuanmaSalva <jsalva@ucm.es>
2021-01-05 12:52:36 +0100

    estados completados

--- PracticasICI/src/main/java/es/ucm/fdi/ici/practica4/demofuzzy/MsPacManActionSelector.java
+++ PracticasICI/src/main/java/es/ucm/fdi/ici/practica4/demofuzzy/MsPacManActionSelector.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 
 import es.ucm.fdi.ici.fuzzy.Action;
 import es.ucm.fdi.ici.fuzzy.ActionSelector;
+import es.ucm.fdi.ici.practica4.demofuzzy.actions.ChaseAction;
 import es.ucm.fdi.ici.practica4.demofuzzy.actions.ChillAction;
 import es.ucm.fdi.ici.practica4.demofuzzy.actions.RunAwayAction;
 
 public class MsPacManActionSelector implements ActionSelector {
 
 	private final Double RUN_AWAY_LIMIT = 20.0;
+	private final Double CHASE_LIMIT = 100.0;
 	MapaInfo mapInfo;
 	
 	public MsPacManActionSelector(MapaInfo map) {
@@ -26,10 +28,10 @@ public class MsPacManActionSelector implements ActionSelector {
 		
 		if(runAway > this.RUN_AWAY_LIMIT)
 			return new RunAwayAction(mapInfo);
-		//else if(map) return new EatPowePillAction(mapInfo);;
+		else if(edible > this.CHASE_LIMIT) 
+			return new ChaseAction(mapInfo);
 		else
 			return new ChillAction(mapInfo);
-			//return new EatPowePillAction(mapInfo);
 	}
 	
 	public void SetMap(MapaInfo map) {
--- PracticasICI/src/main/java/es/ucm/fdi/ici/practica4/demofuzzy/actions/ChaseAction.java
+++ PracticasICI/src/main/java/es/ucm/fdi/ici/practica4/demofuzzy/actions/ChaseAction.java
@@ -1,5 +1,26 @@
 package es.ucm.fdi.ici.practica4.demofuzzy.actions;
 
-public class ChaseAction {
+import es.ucm.fdi.ici.fuzzy.Action;
+import es.ucm.fdi.ici.practica4.demofuzzy.MapaInfo;
+import es.ucm.fdi.ici.practica4.demofuzzy.MapaInfo.interseccion;
+import pacman.game.Game;
+import pacman.game.Constants.MOVE;
 
+public class ChaseAction implements Action{
+	MapaInfo mapInfo; 
+	
+	public ChaseAction(MapaInfo map) {
+		mapInfo = map;
+	}
+	
+	@Override
+	public MOVE execute(Game game) {
+		 
+		return MOVE.NEUTRAL;
+    }
+           
+	public void setMap(MapaInfo map) {
+		mapInfo = map;
+	}
+	
 }
